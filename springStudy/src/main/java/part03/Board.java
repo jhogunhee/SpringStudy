0bb3dcd6b6c50486fd8ba10c2f20011844840c36
조건희 <rjsgml1677@naver.com>
2021-02-24 14:12:52 +0900

    part03 FactoryMethod 이용

--- springStudy/src/main/java/part03/Board.java
+++ springStudy/src/main/java/part03/Board.java
@@ -4,13 +4,6 @@ import lombok.Setter;
 
 public class Board {
 	
-	/*
-	 * @Autowired
-	 * public void setWrite(Write write) {
-	 *   this.write = write; 와 같은 코드가 만들어진다.
-	 * }
-	 */
-	@Setter(onMethod_ = {@Autowired})
     private Write write;
 
     public Board() {
