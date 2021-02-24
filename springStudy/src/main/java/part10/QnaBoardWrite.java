b26807e431fd3d9d585c89d6883c7083e0ae184b
조건희 <rjsgml1677@naver.com>
2021-02-24 14:55:44 +0900

    part10 component스캔을 이용한 주입 2021-02-24

---
+++ springStudy/src/main/java/part10/QnaBoardWrite.java
@@ -0,0 +1,30 @@
+package part10;
+
+import org.springframework.stereotype.Component;
+
+@Component("qnaWrite")
+public class QnaBoardWrite implements Write {
+
+    private String boardName;
+
+    public QnaBoardWrite() {
+        this.init();
+    }
+
+    public void init() {
+        this.setBoardName("Q&A Board");
+    }
+
+    public void doWrite() {
+        System.out.println(this.getBoardName() + " Write down!!");
+    }
+
+    public String getBoardName() {
+        return boardName;
+    }
+
+    public void setBoardName(String boardName) {
+        this.boardName = boardName;
+    }
+
+}
---
+++ springStudy/src/main/java/part10/Write.java
@@ -0,0 +1,7 @@
+package part10;
+
+public interface Write {
+
+    public void doWrite();
+
+}
