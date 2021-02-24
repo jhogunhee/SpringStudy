88d0925a94a93016c720386be6c985bddb67c994
조건희 <rjsgml1677@naver.com>
2021-02-24 16:10:57 +0900

    part14 스프링 설정 파일 없이 Annotation으로 설정하기

---
+++ springStudy/src/main/java/part14/FreeBoardWrite.java
@@ -0,0 +1,29 @@
+package part14;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FreeBoardWrite implements Write {
+
+    private String boardName;
+
+    public FreeBoardWrite() {
+        this.init();
+    }
+
+    public void init() {
+        this.setBoardName("Free Board");
+    }
+
+    public String getBoardName() {
+        return boardName;
+    }
+
+    public void setBoardName(String boardName) {
+        this.boardName = boardName;
+    }
+
+    public void doWrite(String msg) {
+        System.out.println(this.getBoardName() + " Write down!!");
+    }
+}
